4f99949218793af66bf8f5f6f9f4d4436e73e948
MrMahdi313 <m.m.z.m12363@gmail.com>
2019-08-18 06:17:48 -0700

    first structure

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+from .text import Text
+from .config import Config
+from .methods import Methods
+from .keyboard import Keyboard
---
+++ utils/methods.py
@@ -0,0 +1,5 @@
+class Reaction:
+    pass
+
+class Methods(Reaction):
+    pass
